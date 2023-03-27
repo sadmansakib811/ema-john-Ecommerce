8eb225164a068a1845b6bf3068028c13b69e5945
Sadman Sakib <sadmansakib123450@gmail.com>
2023-03-27 23:04:06 +0200

    Header is done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+import './App.css'
+import Header from './components/Header/header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     <Header></Header>
+    </div>
+  )
+}
+
+export default App
