1e2e9762122709c965a63e5ba0d09df150f71dc4
Sadman Sakib <sadmansakib123450@gmail.com>
2023-03-28 23:40:24 +0200

    bug fixed in header

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 
 import './App.css'
-import Header from './components/Header/header'
+import Header from './components/Header/Header'
 import Shop from './Components/Header/Shop/Shop'
 
 function App() {
