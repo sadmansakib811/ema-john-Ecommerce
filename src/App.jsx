6d65c8fef4bb6decdc21f5e098add2575fbc0cd3
Sadman Sakib <34483142+sadmansakib811@users.noreply.github.com>
2023-03-30 03:31:10 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 
 import './App.css'
-import Header from './components/Header/Header'
+import Header from './Components/Header/Header'
 import Shop from './Components/Shop/Shop'
 
 function App() {
