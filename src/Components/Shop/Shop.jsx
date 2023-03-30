af7bda1b66313c4f0629d75855c440fba653488f
Sadman Sakib <sadmansakib123450@gmail.com>
2023-03-30 03:21:32 +0200

    retrieve product from localStorage cart and set it to the cart ui after refresh

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -26,8 +26,24 @@ const [cart, setCart]=useState([])
     // useEffect bebohar korte hbe:
     useEffect(()=>{
         const storedCart = getShoppingCart();
-        console.log(storedCart);
-    },[])
+        const savedCart =[];
+        // step-1: get id.
+        for(const id in storedCart){
+            // step-2: get the product by using id.
+          const addedProduct = products.find(product =>product.id === id)
+        
+        // ai if ta use korsi coz prothom bar data fetch howar karone product ashar agei ai useeffect ta run hoi tokhon shob kisu empty thake but porer bar product change hoile jate abr run hoi tar jonno dependency te [product] disi
+        if(addedProduct){
+            // step-3: get quantity of the product
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+        }
+        } // uporer useState theke products er data nia ansi jate localhost a save thaka id er sathe product match kore exact product tar sob info show kora jai
+        setCart(savedCart);
+    },[products])
+
+
     return (
         
         <div className='shop-container'>
